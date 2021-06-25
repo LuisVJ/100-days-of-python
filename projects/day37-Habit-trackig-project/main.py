bfcdeac8ebe1d8d6ffb6cebb5406acae510c2c4d
Luis Vidal <43793069+LuisVJ@users.noreply.github.com>
2021-06-25 14:40:12 +0200

    Day 37, Habit tracking project complete

---
+++ projects/day37-Habit-trackig-project/main.py
@@ -0,0 +1,71 @@
+import requests
+from dotenv import load_dotenv
+from os import getenv, read
+import datetime
+
+from requests.models import Response
+
+load_dotenv(".env")
+
+PIXELA_TOKEN = getenv("PIXELA_TOKEN")
+USERNAME = "luisv"
+
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": PIXELA_TOKEN,
+    "username": "luisv",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# **** User creation *****
+# response = requests.post(url=pixela_endpoint, json=user_params )
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+headers = {
+    "X-USER-TOKEN": PIXELA_TOKEN,
+}
+
+graph_config = {
+    "id": "graph1",
+    "name": "Reading Graph",
+    "unit": "pages",
+    "type": "int",
+    "color": "sora",
+}
+
+# response = requests.post(url=graph_endpoint, headers=headers, json=graph_config)
+# print(response.text)
+
+reading_graph_endpoint = f"{graph_endpoint}/graph1"
+
+today = datetime.date.today().strftime('%Y%m%d')
+yesterday = datetime.date.today() - datetime.timedelta(days=1)
+yesterday = yesterday.strftime('%Y%m%d')
+
+print(yesterday)
+
+graph1_config = {
+    "date": yesterday,
+    "quantity": "15",
+    "optionalData": "{\"Book\":\"The pragmatic programmer\"}"
+}
+
+# response = requests.post(
+#     url=reading_graph_endpoint, headers=headers, json=graph1_config
+# )
+# print(response.text)
+update_endpoint = f"{reading_graph_endpoint}/{yesterday}"
+
+update_config = {
+    "quantity": "31",
+    "optionalData": "{\"Book\":\"The pragmatic programmer\"}"
+}
+
+response = requests.put(url=update_endpoint, headers=headers, json=update_config)
+
+print(response.text)
